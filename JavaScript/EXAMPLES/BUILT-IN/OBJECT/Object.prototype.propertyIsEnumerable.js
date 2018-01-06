6d619ec8a2121e07432a551329e47ba32403a5f9
Webia1 <eb@ebia.eu>
2018-01-06 18:34:08 +0100

    Objects ready

--- JavaScript/EXAMPLES/BUILT-IN/OBJECT/Object.prototype.propertyIsEnumerable.js
+++ JavaScript/EXAMPLES/BUILT-IN/OBJECT/Object.prototype.propertyIsEnumerable.js
@@ -1,1 +1,11 @@
-console.log ();
+var o = {
+    foo: 'love',
+    bar: [1,2,3]
+};
+
+console.log (
+    o.propertyIsEnumerable('foo'),    // true
+    o.propertyIsEnumerable('bar'),    // true
+    o.propertyIsEnumerable('length'), // false
+    Object.getOwnPropertyDescriptors(o)
+);
--- JavaScript/EXAMPLES/BUILT-IN/OBJECT/Object.values.js
+++ JavaScript/EXAMPLES/BUILT-IN/OBJECT/Object.values.js
@@ -1,1 +1,19 @@
-console.log ();
+var myObject = {
+    d: new Date(),
+    n: Math.PI,
+    j: 0,
+    b: true
+};
+
+console.log (
+    Object
+        .values(myObject)
+        .join('\n'),
+);
+
+/*
+ Sat Jan 06 2018 18:21:04 GMT+0100 (CET)
+ 3.141592653589793
+ 0
+ true
+*/
